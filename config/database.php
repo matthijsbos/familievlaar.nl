f6ddc517e489146ea62ce8ca68ea73009c98b1ea
Matthijs Bos <matthijs_vlaarbos@hotmail.com>
2015-06-11 15:16:40 +0200

    modified database config such that it defaults to Homestead values

--- config/database.php
+++ config/database.php
@@ -54,10 +54,11 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+            // Defaults for Homestead Vagrant VM
+			'host'      => env('DB_HOST', 'localhost:33060'),
+			'database'  => env('DB_DATABASE', 'homestead'),
+			'username'  => env('DB_USERNAME', 'homestead'),
+			'password'  => env('DB_PASSWORD', 'secret'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
